5b80c7bcd9bf05072c3b6de5e9370c44e4e692bd
Jingjing Duan <jduan@twitter.com>
2016-02-10 20:48:52 -0800

    revert HelloWorldController.scala

--- examples/hello-world/src/main/scala/com/twitter/hello/HelloWorldController.scala
+++ examples/hello-world/src/main/scala/com/twitter/hello/HelloWorldController.scala
@@ -1,18 +1,16 @@
 package com.twitter.hello
 
 import com.twitter.finagle.http.Request
-import com.twitter.finagle.http.Response
 import com.twitter.finatra.http.Controller
 
 class HelloWorldController extends Controller {
 
   get("/hi") { request: Request =>
     info("hi")
-    java.util.Objects.toString(null) + "Hello " + request.params.getOrElse("name", "unnamed")
+    "Hello " + request.params.getOrElse("name", "unnamed")
   }
 
   post("/hi") { hiRequest: HiRequest =>
     "Hello " + hiRequest.name + " with id " + hiRequest.id
   }
 }
-
