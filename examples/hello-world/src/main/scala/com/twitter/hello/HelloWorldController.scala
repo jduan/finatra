f24e3fc2174b63b7c90263155be9a3a43f940b7e
Jingjing Duan <jduan@fitbit.com>
2016-05-19 21:46:48 -0700

    example to show Finatra controllers in Scala don't block Finagle main threads

--- examples/hello-world/src/main/scala/com/twitter/hello/HelloWorldController.scala
+++ examples/hello-world/src/main/scala/com/twitter/hello/HelloWorldController.scala
@@ -1,13 +1,23 @@
 package com.twitter.hello
 
+import java.util.concurrent.Executors
+
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import com.twitter.util.{Future, FuturePools, FuturePool}
 
 class HelloWorldController extends Controller {
 
+  val futurePool = FuturePools.newInterruptibleFuturePool(Executors.newFixedThreadPool(50))
+
   get("/hi") { request: Request =>
     info("hi")
-    "Hello " + request.params.getOrElse("name", "unnamed")
+    val ret: Future[String] = futurePool {
+      Thread sleep 5000
+      "Hello " + request.params.getOrElse("name", "unnamed")
+    }
+
+    ret
   }
 
   post("/hi") { hiRequest: HiRequest =>
